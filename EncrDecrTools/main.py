2dc92b86d8a6374f0a5e95f2121bf0760796a647
Nightwalker495 <milan.ondrasovic@gmail.com>
2018-10-14 08:56:30 +0200

    Basic directory skeleton.

---
+++ EncrDecrTools/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/evn python
+# Author: Milan Ondrasovic <milan.ondrasovic@gmail.com>
+#
+# This file is path of EncrDecrTools (Encryption/Decryption Tools)
+# related to the cryptography classes.
+
+import sys
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ README.md
@@ -0,0 +1,7 @@
+# Cryptography - encryption/decryption tools
+
+This project contains various **encryption/decryption tools** related to cryptography classes.
+
+## Caesar Cipher
+
+## Vigenere Cipher
