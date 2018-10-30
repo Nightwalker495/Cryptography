ada19ae44a30ea593bd6427fa4ece6bfcd93c348
Nightwalker495 <milan.ondrasovic@gmail.com>
2018-10-30 18:26:59 +0100

    Refactoring.

--- CryptoTools/hill/hill.py
+++ CryptoTools/hill/hill.py
@@ -10,72 +10,63 @@ import click
 import numpy as np
 
 
-class TrigramHillCipher:
+class HillCipher:
 
-    MATRIX_SIZE = 9
-    TRIGRAM_SIZE = 3
     MOD = 26
 
-    def __init__(self, plain_text_start):
-        self.__plain_text_start = plain_text_start
+    def __init__(self, plain_text_start, cipher_text, block_size):
+        if block_size < 1:
+            raise ValueError('block size must be at least 1')
+        if len(plain_text_start) < block_size ** 2:
+            raise ValueError('insufficient number of plain text characters')
+        if len(cipher_text) % 3 != 0:
+            raise ValueError('cipher text len. is not a multiple of block size')
 
-    def decrypt(self, cipher_text):
-        cipher_text_order_vals = \
+        self.__plain_text_start_ord =\
+            self.convert_text_to_letter_order_vals(plain_text_start)
+        self.__cipher_text_ord =\
             self.convert_text_to_letter_order_vals(cipher_text)
-        cipher_text_trigrams = list(self.separate_into_groups(
-            cipher_text_order_vals, self.TRIGRAM_SIZE))
+        self.__block_size = block_size
 
-        decryption_matrix = self.__find_decryption_matrix(cipher_text_trigrams)
+    def decrypt(self):
+        decryption_matrix = self.__find_decryption_matrix()
 
-        plain_text_order_vals = []
-        for trigram in cipher_text_trigrams:
-            plain_text = np.matmul(decryption_matrix, trigram)
-            plain_text = np.remainder(plain_text, np.full(plain_text.shape, 26))
-            for p in plain_text:
-                plain_text_order_vals.append(int(p))
+        plain_text_ord = []
+        blocks = self.separate_into_blocks(self.__cipher_text_ord,
+                                           self.__block_size)
+        for block in blocks:
+            plain_text_vector = np.matmul(decryption_matrix, block)
+            plain_text_vector = self.__matrix_elements_mod(plain_text_vector)
+            
+            for item in plain_text_vector:
+                plain_text_ord.append(int(item))
 
-        return self.convert_letter_order_vals_to_text(plain_text_order_vals)
+        return self.convert_letter_order_vals_to_text(plain_text_ord)
 
     @staticmethod
     def convert_text_to_letter_order_vals(text):
         if not text.isalpha():
             raise ValueError('text contains a non-alpha character')
-        return [
-            ord(letter) - (ord('A') if letter.isupper() else ord('a'))
-            for letter in text
-        ]
+        return [HillCipher.get_letter_order(letter) for letter in text]
 
     @staticmethod
     def convert_letter_order_vals_to_text(order_vals):
         return ''.join(chr(val + ord('A')) for val in order_vals)
 
     @staticmethod
-    def separate_into_groups(data, group_size):
-        for i in range(0, len(data), group_size):
-            yield data[i:i + group_size]
+    def get_letter_order(letter):
+        return ord(letter) - (ord('A') if letter.isupper() else ord('a'))
 
     @staticmethod
-    def matrix_mod_inverse(matrix, mod):
-        inverse, det = np.linalg.inv(matrix), np.linalg.det(matrix)
-        inv_det_mul = inverse * det
-
-        det_mod_inv = TrigramHillCipher.mod_inv(int(round(det)) % mod, mod)
-
-        matrix_det_mov_inv_mul = inv_det_mul * det_mod_inv
-        matrix_rounded = matrix_det_mov_inv_mul.round()
-
-        size = TrigramHillCipher.MATRIX_SIZE
-        return np.remainder(matrix_rounded, np.full((size, size), mod))
-
-    @staticmethod
-    def matrix_mod(matrix, mod):
-        return np.remainder(matrix, np.full(matrix.shape, mod))
+    def separate_into_blocks(data, block_size):
+        for i in range(0, len(data), block_size):
+            yield data[i:i + block_size]
 
     @staticmethod
     def mod_inv(a, m):
         if a < 0:
             a = m - a
-        g, x, y = TrigramHillCipher.__egcd(a, m)
+        g, x, y = HillCipher.__egcd(a, m)
         if g != 1:
             raise ValueError('modular inverse does not exist')
         return x % m
@@ -85,60 +76,59 @@ class TrigramHillCipher:
         if a == 0:
             return b, 0, 1
         else:
-            g, y, x = TrigramHillCipher.__egcd(b % a, a)
+            g, y, x = HillCipher.__egcd(b % a, a)
             return g, x - (b // a) * y, y
 
     @staticmethod
-    def __build_matrix_from_ciphertext(trigram_1, trigram_2, trigram_3):
-        size = TrigramHillCipher.MATRIX_SIZE
-        matrix = np.zeros(shape=(size, size))
+    def __matrix_mod_inverse(matrix):
+        inverse, det = np.linalg.inv(matrix), np.linalg.det(matrix)
+        inv_det_mul = inverse * det
 
-        t = trigram_1
-        matrix[0][0], matrix[0][1], matrix[0][2] = t[0], t[1], t[2]
-        matrix[1][3], matrix[1][4], matrix[1][5] = t[0], t[1], t[2]
-        matrix[2][6], matrix[2][7], matrix[2][8] = t[0], t[1], t[2]
+        mod = HillCipher.MOD
+        det_mod_inv = HillCipher.mod_inv(int(round(det)) % mod, mod)
 
-        t = trigram_2
-        matrix[3][0], matrix[3][1], matrix[3][2] = t[0], t[1], t[2]
-        matrix[4][3], matrix[4][4], matrix[4][5] = t[0], t[1], t[2]
-        matrix[5][6], matrix[5][7], matrix[5][8] = t[0], t[1], t[2]
+        matrix_det_mov_inv_mul = inv_det_mul * det_mod_inv
+        matrix_rounded = matrix_det_mov_inv_mul.round()
 
-        t = trigram_3
-        matrix[6][0], matrix[6][1], matrix[6][2] = t[0], t[1], t[2]
-        matrix[7][3], matrix[7][4], matrix[7][5] = t[0], t[1], t[2]
-        matrix[8][6], matrix[8][7], matrix[8][8] = t[0], t[1], t[2]
+        return HillCipher.__matrix_elements_mod(matrix_rounded)
+
+    @staticmethod
+    def __matrix_elements_mod(matrix):
+        return np.remainder(matrix, np.full(matrix.shape, HillCipher.MOD))
+
+    def __build_matrix_from_cipher_text(self):
+        bs = self.__block_size
+        matrix_size = bs ** 2
+        matrix = np.zeros(shape=(matrix_size, matrix_size))
+
+        for row_offset in range(bs):
+            for i in range(matrix_size):
+                row = row_offset + ((i // bs) * bs)
+                col = (row_offset * bs) + (i % bs)
+                matrix[row][col] = self.__cipher_text_ord[i]
 
         return matrix
 
-    def __find_decryption_matrix(self, cipher_text_trigrams):
-        cipher_text_matrix = self.__build_matrix_from_ciphertext(
-            cipher_text_trigrams[0], cipher_text_trigrams[1],
-            cipher_text_trigrams[2])
-        cipher_text_matrix_inv = self.matrix_mod_inverse(cipher_text_matrix,
-                                                         self.MOD)
+    def __find_decryption_matrix(self):
+        cipher_text_matrix = self.__build_matrix_from_cipher_text()
+        cipher_text_matrix_inv = self.__matrix_mod_inverse(cipher_text_matrix)
 
-        p = self.convert_text_to_letter_order_vals(
-            self.__plain_text_start)
-        plain_text_vector = np.array([p[0], p[1], p[2], p[3], p[4], p[5],
-                                      p[6], p[7], p[8]])
+        plain_text_ord_vector = np.array(
+            self.__plain_text_start_ord[:self.__block_size ** 2])
+        res_vector = np.matmul(cipher_text_matrix_inv, plain_text_ord_vector)
+        res_matrix = res_vector.reshape((self.__block_size, self.__block_size))
 
-        res_vector = np.matmul(cipher_text_matrix_inv, plain_text_vector)
-        res_matrix = np.array([
-            [res_vector[0], res_vector[1], res_vector[2]],
-            [res_vector[3], res_vector[4], res_vector[5]],
-            [res_vector[6], res_vector[7], res_vector[8]]
-        ])
-        return TrigramHillCipher.matrix_mod(res_matrix, TrigramHillCipher.MOD)
+        return HillCipher.__matrix_elements_mod(res_matrix)
 
 
 @click.command()
 @click.argument('plain_text_start')
 def main(plain_text_start):
-    hill_cipher = TrigramHillCipher(plain_text_start)
-
     for line in sys.stdin.readlines():
         line_stripped = line.strip()
-        decrypted_text = hill_cipher.decrypt(line_stripped)
+        hill_cipher = HillCipher(plain_text_start, line_stripped, 3)
+
+        decrypted_text = hill_cipher.decrypt()
         print('"{}" --> "{}"'.format(line_stripped, decrypted_text))
 
     return 0
