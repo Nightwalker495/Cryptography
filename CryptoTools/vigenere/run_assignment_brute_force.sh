8834f98074db767a0e04d75e3b81357e81708e47
Nightwalker495 <milan.ondrasovic@gmail.com>
2018-10-30 18:37:19 +0100

    Directory reorganization.

--- CryptoTools/vigenere/run_assignment_brute_force.sh
+++ CryptoTools/vigenere/run_assignment_brute_force.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 vigenere.py --brute-force 20,29 < ../input/vigenere/text4_enc.txt > ./assignment_4/brute_force_output.txt
+python3 vigenere.py --brute-force 20,29 < ./assignment_4/text4_enc.txt > ./assignment_4/brute_force_output.txt
